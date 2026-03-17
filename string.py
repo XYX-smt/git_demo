3bcb37042e63700fb6e0c9d4bc66c819c23b1616
XYX-smt <yixing_xiong@foxmail.com>
2026-03-17 09:13:45 +0800

    loop test

--- string.py
+++ string.py
@@ -1,4 +1,8 @@
-s1 = 72
-s2 = 85
-r = ((s2-s1) * 100)/s1
-print(f'{r:.1f}%')
+# s1 = 72
+# s2 = 85
+# r = ((s2-s1) * 100)/s1
+# print(f'{r:.1f}%')
+
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print(f'hello, {name}')
